92f9fdbb08d702d39f5c88b1c352adace1a8e28f
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2023-04-12 03:21:48 +0200

    Add Implementation with PictureBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            groupBox1 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(674, 450);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(9, 465);
+            groupBox1.Margin = new Padding(0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(100, 100);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Interval";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(6, 69);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(36, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.Text = "2s";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Checked = true;
+            radioButton2.Location = new Point(6, 44);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(36, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "1s";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 19);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(59, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.Text = "500ms";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(179, 478);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 80);
+            button1.TabIndex = 3;
+            button1.Text = "Prev";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(285, 478);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 80);
+            button2.TabIndex = 4;
+            button2.Text = "Pause";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(391, 478);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 80);
+            button3.TabIndex = 5;
+            button3.Text = "Next";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(562, 468);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 100);
+            button4.TabIndex = 6;
+            button4.Text = "Exit";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(674, 577);
+            ControlBox = false;
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Slideshow";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,94 @@ namespace Slideshow
 {
     public partial class Form1 : Form
     {
+        private List<Image> images;
+        private int interval = 1000;
+        private int index = 0;
+        private bool isPlaying = true;
         public Form1()
         {
             InitializeComponent();
+            images = new List<Image>
+            {
+                Properties.Resources.Image1,
+                Properties.Resources.Image2,
+                Properties.Resources.Image3,
+                Properties.Resources.Image4,
+                Properties.Resources.Image5,
+            };
+        }
+
+        private void SetPicture()
+        {
+            pictureBox1.Image = images[index];
+        }
+
+        private void SetTimer(int interval)
+        {
+            timer1.Interval = this.interval = interval;
+            if (isPlaying) timer1.Start(); else timer1.Stop();
+        }
+
+        private void TogglePlaying(bool state)
+        {
+            isPlaying = state;
+            button2.Text = isPlaying ? "Pause" : "Play";
+            SetTimer(interval);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetPicture();
+            SetTimer(interval);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!isPlaying) return;
+            index++;
+            if (index >= images.Count) index = 0;
+            SetPicture();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            SetTimer(500);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            SetTimer(1000);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            SetTimer(2000);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TogglePlaying(false);
+            index--;
+            if (index < 0) index = images.Count - 1;
+            SetPicture();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TogglePlaying(!isPlaying);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TogglePlaying(false);
+            index++;
+            if (index >= images.Count) index = 0;
+            SetPicture();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
